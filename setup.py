d9425a9107b5df4a9f670001246a9bc436dfcd8f
Justin <justin@sisudata.com>
2022-06-30 20:27:09 +0000

    betterproto as a dep

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-setup_info = dict(
+setup(
     name='sisu_api',
     version='0.0.1',
-    packages=find_packages(include=['sisu_api', 'sisu_api.*'])
-
+    packages=find_packages(include=['sisu_api', 'sisu_api.*']),
+    install_requires=[
+        'betterproto>=2.0.0b4',
+    ],
 )
-
-setup(**setup_info)
